23e815c79d647b259a7c7f07cdd611b08c1fbc75
Melany Dalama <melinahir.md@gmail.com>
2023-08-04 13:42:13 -0300

    Creación de proyecto, package y class Empleado. Aplicación de encapsulamiento.

---
+++ TP1_Grupo_2_1/src/Ejercicio1/Empleado.java
@@ -0,0 +1,42 @@
+package Ejercicio1;
+
+public class Empleado {
+	
+	//atributos
+	
+	private int id;
+	private String nombre;
+	private int edad;
+	
+
+	
+	//constructores
+	
+	
+	
+	
+	
+	//gets y sets
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+
+}
