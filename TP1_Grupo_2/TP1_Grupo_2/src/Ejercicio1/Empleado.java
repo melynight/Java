127009850ac9f27df01665a3b6d3d0fd527830b1
DESKTOP-9U90DKA\lucia <lucianovitale00@gmail.com>
2023-08-08 23:46:07 -0300

    Creacion de class Principal contenedora del main y un array de objetos Empleado Punto 7

---
+++ TP1_Grupo_2/TP1_Grupo_2/src/Ejercicio1/Empleado.java
@@ -0,0 +1,47 @@
+package Ejercicio1;
+
+public class Empleado {
+	//atributos
+	private final int id;
+	private String nombre;
+	private int edad;
+	private static int cont=999;
+	
+	//constructores
+	
+	public Empleado(){
+		cont++;
+		this.id = cont;
+		this.nombre="sin nombre";
+		this.edad=99;
+	}
+
+	public Empleado(String nombre,int edad){
+		cont++;
+		this.id = cont;
+		this.nombre=nombre;
+		this.edad=edad;
+	}
+	
+	//gets y sets
+	public int getId() {
+		return id;
+	}
+	/*public void setId(int id) {
+		this.id = id;
+	}*/
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	//metodos
+}
---
+++ TP1_Grupo_2/src/Ejercicio1/Principal.java
@@ -0,0 +1,28 @@
+package Ejercicio1;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Empleado[] empleados = new Empleado[5];
+		
+		empleados[0] = new Empleado();
+		empleados[1] = new Empleado("Jose",33);
+		empleados[2] = new Empleado();
+		empleados[3] = new Empleado("Perla",19);
+		empleados[4] = new Empleado("Carlos",55);
+		
+		
+		for (Empleado emp : empleados ) {
+            System.out.println(emp);
+        }
+		
+		
+								}
+					
+														}
+
+	
+
+	
